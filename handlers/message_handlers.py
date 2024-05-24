8e74aa8da2cc79c43eba807e195ac0da999e95f5
Daniil Zhilin <d.zhilin2013@yandex.ru>
2024-05-24 20:03:45 +0300

    update

--- handlers/message_handlers.py
+++ handlers/message_handlers.py
@@ -4,23 +4,11 @@ from config.openai_client import client
 async def chatgpt_reply(update: Update, context):
     # текст входящего сообщения
     text = update.message.text
-
-    f = open("analyst_virtual_assistant/prompt.txt", "r")
-    s = f.read()
-
-    # f = open('/root/cib_enjoyers/database.md', 'r')
-    # htmlmarkdown=markdown.markdown( f.read() )
-
-    # htmlmarkdown = htmlmarkdown.replace('\*', '')
-    # htmlmarkdown = htmlmarkdown.replace('\#', '')
-    # htmlmarkdown = htmlmarkdown.replace('\-', '')
-    
-    text_1 = "Тебе дана структура данных компании:" + s + "ответь на вопрос:" + text
     
     # запрос
     response = client.chat.completions.create(
         model="gpt-3.5-turbo",
-        messages=[{"role": "user", "content": text_1}],
+        messages=[{"role": "user", "content": text}],
         max_tokens=1024,
         temperature=0.5,
     )
