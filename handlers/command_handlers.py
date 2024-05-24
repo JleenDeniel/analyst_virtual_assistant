7a63a54a723b52a1e9035d9bd14bf1e4110936e9
root <root@team-9>
2024-05-24 19:50:12 +0000

    add logging

--- handlers/command_handlers.py
+++ handlers/command_handlers.py
@@ -1,4 +1,63 @@
-import json
+from telegram import Update
+from config.openai_client import client
+
+
+async def chatgpt_reply(update: Update, context):
+    # текст входящего сообщения
+    text = update.message.text
+
+    # f = open("/root/cib_enjoyers/prompt.txt", "r")
+    # s = f.read()
+
+    # with open('/root/cib_enjoyers/logfile.log') as f_1:
+    #     for line in f_1:
+    #         pass
+    #     last_line = line
+
+    with open('logfile.log') as f_1:
+        for line in f_1:
+            pass
+        last_line = line
+
+    last_line = last_line.replace('\n', '')
+
+    helper = ''
+    
+    if 'DA' in last_line:
+        helper = 'Отвечай максимально просто, не нужно объяснять каждую деталь'
+    elif 'DS' in last_line:
+        helper = 'После ответа на вопрос ниже объясни каждый финансовый термин и бизнес значимость'
+    if 'Intern' in last_line:
+        helper = 'Отвечай максимально подробно, чтобы понял даже ребенок'
+    if 'Buddy' in last_line:
+        helper = 'Подскажи, как объяснить этот ответ новичку'
+    
+    # f = open('/root/cib_enjoyers/database.md', 'r')
+    # htmlmarkdown=markdown.markdown( f.read() )
+
+    # htmlmarkdown = htmlmarkdown.replace('\*', '')
+    # htmlmarkdown = htmlmarkdown.replace('\#', '')
+    # htmlmarkdown = htmlmarkdown.replace('\-', '')
+    
+    # text_1 = "Тебе дана структура данных компании:" + s + "ответь на вопрос:" + text + '\n'+ helper
+    text_1 = "Тебе дана структура данных компании:" + "ответь на вопрос:" + text + '\n'+ helper
+    
+    # запрос
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": text_1}],
+        max_tokens=1024,
+        temperature=0.5,
+    )
+
+    # ответ
+    reply = response.choices[0].message.content.strip()
+
+    # перенаправление ответа в Telegram
+    await update.message.reply_text(reply)   
+    
+    print("user:", text)
+    print("assistant:", reply)import json
 
 import logging
 
